4392fdc6ee8c6c49a548b276090b7d14c2fabe58
leonelvalentinn <leonelvalentinn@gmail.com>
2023-07-12 15:53:39 -0600

    Banner y nueva ubicacion

---
+++ js/index-modal.js
@@ -0,0 +1,10 @@
+const indexModal = document.getElementById("indexModal");
+const myTimeout = setTimeout(myModal, 3000);
+
+function myModal() {
+  indexModal.classList.add("show-index-modal");
+  const cerrarIndexModal = document.getElementById("cerrarIndexModal");
+  cerrarIndexModal.addEventListener("click", () => {
+    indexModal.classList.remove("show-index-modal");
+  });
+}
